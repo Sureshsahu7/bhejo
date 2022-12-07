9f57879d2646ff4c25892dc06180b46251dc23e1
Aswin-Kumar66 <77041105+Aswin-Kumar66@users.noreply.github.com>
2022-12-07 13:03:38 +0530

    build

---
+++ client/build/precache-manifest.10b2523d60f73284b836f2346483658e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fde30ff128673656fc8fd4355c3328c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2933677f3e2ec793d49c",
+    "url": "/static/css/2.b73924fc.chunk.css"
+  },
+  {
+    "revision": "1e67f874ec7b39154b63",
+    "url": "/static/css/main.36ee8a26.chunk.css"
+  },
+  {
+    "revision": "2933677f3e2ec793d49c",
+    "url": "/static/js/2.3b58d080.chunk.js"
+  },
+  {
+    "revision": "1e67f874ec7b39154b63",
+    "url": "/static/js/main.75ed0a38.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "24c5e005fff35cdd67e7feab35560029",
+    "url": "/static/media/Wallpaper_1.24c5e005.jpg"
+  },
+  {
+    "revision": "109bf96cbe6dcb25907f7afaf0e392fd",
+    "url": "/static/media/Wallpaper_2.109bf96c.jpg"
+  },
+  {
+    "revision": "c511a19b2bf9784313821015a9dcbd4f",
+    "url": "/static/media/Wallpaper_3.c511a19b.jpg"
+  },
+  {
+    "revision": "38dffad1bff88103f05e11bd5d83620c",
+    "url": "/static/media/black_checks.38dffad1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
